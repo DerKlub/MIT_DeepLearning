9e8f610dde49f003da4aa28ce25628b7a1bd8bcf
mkieffer1107 <mkieffer1107@gmail.com>
2021-05-23 20:12:47 -0400

    Update Lecture1_DenseLayer.py

--- Lecture1_DenseLayer.py
+++ Lecture1_DenseLayer.py
@@ -1,1 +1,18 @@
+import tensorflow as tf
 
+class MyDenseLayer(tf.keras.layers.Layer):
+    def __init__(self, input_dim, output_dim):
+        super(MyDenseLayer, self).__init__()
+
+        #Initialize weights and biases
+        self.W = self.add_weight([input_dim, output_dim])
+        self.b = self.add_weight([1, output_dim])
+
+    def call(self, inputs):  #defining forward propagation
+        #Forward propagate the inputs
+        z = tf.matmu1(inputs, self.W) + self.b
+
+        #Feed through a non-linear activation
+        output = tf.math.sigmoid(z)
+
+        return output
