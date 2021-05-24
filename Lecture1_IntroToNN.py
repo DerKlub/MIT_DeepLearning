fbd1b4a3dd92170b3bbd913adbc2f0cd981fe513
mkieffer1107 <mkieffer1107@gmail.com>
2021-05-23 22:42:04 -0400

    Update Lecture1_IntroToNN.py

--- Lecture1_IntroToNN.py
+++ Lecture1_IntroToNN.py
@@ -60,3 +60,62 @@ loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, predicted))
 loss = tf.reduce_mean(tf.square(tf.subtract(y, predicted)))  #rigorous
 #or
 loss = tf.keras.losses.MSE(y, predicted)  #shortcut
+
+
+
+
+
+
+#Gradient Descent
+
+weights = tf.Variable([tf.random.normal()])  #initial weights randomized
+
+while True:
+    with tf.GradientTape() as g:
+        loss = compute_loss(weights)
+        gradient = g.gradient(loss, weights)
+    
+    weights = weights - lr * gradient
+
+
+
+#Various Gradient Descent Algorithms:
+
+tf.keras.optimizers.SGD   #stochastic
+tf.keras.optimizers.Adam  
+tf.keras.optimizers.Adadelta 
+tf.keras.optimizers.Adagrad
+tf.keras.optimizers.RMSProp
+
+
+
+
+
+
+#PUTTING IT ALL TOGETHER
+
+model = tf.keras.Sequential([...])
+
+optimizer = tf.keras.optimizers.SGD() #pick favorite optimizer
+
+while True:
+    #forward pass through the net
+    prediction = model(x)
+
+    with tf.GradientTape() as tape:
+        #compute loss
+        loss = compute_loss(y, prediction)
+    
+    #update the weights using the gradient
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimzer.apply_gradients(zip(grads, model.trainable_variables))
+
+
+
+#Dropout
+
+#During training, randomly set some activations to 0
+#typically drop 50% of activations in layer
+#forces network to not rely on any 1 node
+
+tf.keras.layers.Dropout(p=0.5)
