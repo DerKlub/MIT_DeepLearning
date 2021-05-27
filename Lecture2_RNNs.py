715a4eb3a442d59804fb8bba825536cbe5796bf0
mkieffer1107 <mkieffer1107@gmail.com>
2021-05-26 21:01:13 -0400

    Update Lecture2_RNNs.py

--- Lecture2_RNNs.py
+++ Lecture2_RNNs.py
@@ -1,1 +1,13 @@
+#RNN Intuition  (pseudocode)
 
+my_rnn = Rnn()
+hidden_state = [0, 0, 0, 0]
+
+sentence = ["I", "love", "recurrent", "neural"]
+
+for word in sentence:
+    prediction, hidden_state = my_rnn(word, hidden_state)
+    #feeding current word and previous hidden state into RNN
+    #this generates prediction and updates hidden state
+
+next_word_prediction = prediction
